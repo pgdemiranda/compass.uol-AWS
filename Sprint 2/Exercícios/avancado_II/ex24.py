6ae03b7ba5f9b5213f26e5d030d2e3727c51a55c
Pablo Miranda <pgdemiranda@gmail.com>
2025-04-30 22:09:25 -0300

    amostragem das evidências dos exercícios Avançados

--- Sprint 2/Exercícios/avancado_II/ex24.py
+++ Sprint 2/Exercícios/avancado_II/ex24.py
@@ -7,11 +7,8 @@ with open('estudantes.csv', 'r') as planilha:
 for aluno in lista_alunos:
     nome = aluno[0]
     notas = list(map(float, aluno[1:]))
-
-    top_3 = sorted(notas, reverse=True)[:3]
-
-    media = round(sum(top_3) / 3, 2)
-
-    notas_mod = [int(nota) if nota.is_integer() else round(nota, 1) for nota in top_3]
+    top_3_notas = sorted(notas, reverse=True)[:3]
+    media = round(sum(top_3_notas) / 3, 2)
+    notas_mod = [int(nota) if nota.is_integer() else round(nota, 1) for nota in top_3_notas]
 
     print(f"Nome: {nome} Notas: {notas_mod} Média: {media}")
